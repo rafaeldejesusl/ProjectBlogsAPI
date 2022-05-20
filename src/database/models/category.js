1e09eab0feaa5b1a418cfd84d9c72cc56ba0dcfd
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2022-05-20 19:54:54 -0300

    Requisito 7

---
+++ src/database/models/category.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    name: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+  });
+
+  return Category;
+};
